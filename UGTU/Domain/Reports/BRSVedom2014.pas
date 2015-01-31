b4963eb8b92979c538d82a58d3287bf4b0d7f0e4
kimspark <sir.k.spark@gmail.com>
2015-01-31 12:31:50 +0300

    commit 517

--- UGTU/Domain/Reports/BRSVedom2014.pas
+++ UGTU/Domain/Reports/BRSVedom2014.pas
@@ -1,157 +1,128 @@
 unit BRSVedom2014;
 
 interface
+
 uses
-Classes, dialogs ,SysUtils, ExcelXP, Barcode, Contnrs,
-XIntf, CommonIntf, ReportsBase, db, adodb, ExtCtrls,
- udmUspevaemost, ComCtrls, uAverageBalls, Vedomost2014,Assemly_Report2014;
+  Classes, dialogs, SysUtils, ExcelXP, Barcode, Contnrs,
+  XIntf, CommonIntf, ReportsBase, db, adodb, ExtCtrls,
+  udmUspevaemost, ComCtrls, uAverageBalls, Vedomost2014, Assemly_Report2014;
+
 type
-    TBRS2014VedomostReport = class(TExcelReportBase)
-    private
-       FReport :TVedomost;
-    protected
-       procedure Execute;override;
-       function GetTotalSteps:Integer;override;
-    public
-    property  Report:TVedomost  read  FReport;
-       constructor Create (report:TVedomost);
+  TBRS2014VedomostReport = class(TExcelReportBase)
+  private
+    FReport: TVedomost;
+  protected
+    procedure Execute; override;
+    function GetTotalSteps: Integer; override;
+  public
+    property Report: TVedomost read FReport;
+    constructor Create(Report: TVedomost);
 
+  end;
 
-    end;
 implementation
+
+uses ConstantRepository;
 
 { TBRS2014Report }
 
-
-constructor TBRS2014VedomostReport.Create(report: TVedomost);
+constructor TBRS2014VedomostReport.Create(Report: TVedomost);
 begin
-  FReport:=report;
+  FReport := Report;
 end;
 
-procedure  TBRS2014VedomostReport.Execute();
+procedure TBRS2014VedomostReport.Execute();
 var
   I: Integer;
-num: Integer;
-Rng:Variant;
-first_str:Integer;
-dir_inst,copystr1,copystr2,fio:string;
-posit:integer;
-//var report:TAssemly_Report;
-//   result_report:TVedomost;
+  num: Integer;
+  Rng: Variant;
+  first_str: Integer;
+  dir_inst, copystr1, copystr2, fio: string;
+  posit: Integer;
 begin
   inherited;
-  Replace('#institut#',FReport.Institute);
-  Replace('#grup#',FReport.grup);
-  if (FReport.ik_vid_zan<>56)  then
-      begin
-      first_str:=15;
-      Replace('#sem#',inttostr(FReport.num_s));
-      Replace('#disc#',FReport.disc);
-      Replace('#date_zach#','');
-      Replace('#ekz_prep#','');
-      end
+  Replace('#institut#', FReport.Institute);
+  Replace('#grup#', FReport.grup);
+  if (FReport.ik_vid_zan <> GOS_EXAM) then
+  begin
+    first_str := 15;
+    Replace('#sem#', inttostr(FReport.num_s));
+    Replace('#disc#', FReport.disc);
+    Replace('#date_zach#', '');
+    Replace('#ekz_prep#', '');
+  end
   else
+  begin
+    first_str := 16;
+    Replace('#f_ob#', FReport.f_obuch);
+    Replace('#spec#', FReport.spec);
+    Replace('#NAME_GOS#', AnsiUpperCase(FReport.disc));
+  end;
+
+  Replace('#date_ekz#', '');
+  dir_inst := FReport.dir_inst;
+  posit := Pos(' ', dir_inst);
+  copystr1 := Copy(dir_inst, posit + 1, Length(dir_inst));
+  copystr2 := Copy(dir_inst, 1, posit - 1);
+  copystr1 := copystr1 + ' ' + copystr2;
+  Replace('#dir_inst#', FReport.dir_inst);
+
+  num := 0;
+  for I := first_str to first_str + FReport.Students.Count - 1 do
+  begin
+    if FReport.Students[num] <> Nil then
+    begin
+
+      Items[I, 1] := num + 1;
+      fio := FReport.Students[num].LastName;
+      copystr1 := Copy(FReport.Students[num].FirstName, 1, 1);
+      copystr2 := Copy(FReport.Students[num].Otch, 1, 1);
+      Items[I, 2] := fio + ' ' + copystr1 + '.' + copystr2 + '.';
+      if (FReport.ik_vid_zan <> GOS_EXAM) then
       begin
-      first_str:=16;
-      Replace('#f_ob#',FReport.f_obuch);
-      Replace('#spec#',FReport.spec);
-      Replace('#NAME_GOS#',AnsiUpperCase(FReport.disc));
-      end;
-//  if (FReport.ik_vid_zan=6) then
-//      begin
-//       Replace('#date_ekz#',DateTimeToStr(FReport.date));
-//       Replace('#date_zach#','');
-//  end
-//  else
-// if (FReport.ik_vid_zan=7) then
-//      begin
-//       Replace('#date_ekz#','');
-//       Replace('#date_zach#',DateTimeToStr(FReport.date));
-//     end
-//      else
-//      begin
-
-      Replace('#date_ekz#','');
-  //    end;
-    dir_inst:=FReport.dir_inst;
-    posit:=Pos(' ', dir_inst);
-    copystr1:=Copy(dir_inst,posit+1,Length(dir_inst));
-    copystr2:=Copy(dir_inst,1,posit-1);
-    copystr1:=copystr1+' '+copystr2;
-    Replace('#dir_inst#',FReport.dir_inst);
-
-
-
-  num:=0;
-  for I := first_str to first_str+FReport.Students.Count-1 do
-   begin
-   if FReport.Students[num]<>Nil then
-       begin
-
-    //  GetRange(Cells[1,1],  Cells[1,10]).Insert;
-     //  Range['A1','A1'].Insert();
-      // ActiveSheet.Rows.Item[2,2].Insert(xlDown, xlFormatFromLeftOrAbove);
-
-//         ActiveSheet.Rows.Item[2,2].Select;
-//       Selection.Insert(xlDown, xlFormatFromLeftOrAbove);
-
-   //   ActiveSheet.Rows('A'+1).Insert();
-//      Rng.Select;
-       //ActiveSheet.Rows[I].Select;
-    //Selection.Insert(Shift :=I);
-//       Range[Cells[I,1], Cells[I,10]].Select;
-//       Selection.Borders.LineStyle:=1;
-      Items[I, 1] := num+1;
-      fio:=FReport.Students[num].LastName;
-      copystr1:=Copy(FReport.Students[num].FirstName,1,1);
-      copystr2:=Copy(FReport.Students[num].Otch,1,1);
-      Items[I, 2] := fio+ ' '+copystr1+'.'+copystr2+'.';
-       if (FReport.ik_vid_zan<>56)  then
-       begin
-          if (FReport.Is_brs) then
-            begin
-            Items[I, 6]:= IntToStr( FReport.Students[num].balls);
-            end;
-       end;
-
-        if I<>first_str+FReport.Students.Count-1 then
+        if (FReport.Is_brs) then
         begin
-        if (FReport.Is_brs) or (FReport.ik_vid_zan=56) then
-            begin
-         Range['A'+inttostr(i+1),'J'+inttostr(i+1)].Insert(xlDown, xlFormatFromLeftOrAbove);
-          if (FReport.ik_vid_zan=56) then
-             begin
-         Range['B'+inttostr(i+1),'D'+inttostr(i+1)].Select;
-         Selection.MergeCells:=True;
-         Range['E'+inttostr(i+1),'G'+inttostr(i+1)].Select;
-         Selection.MergeCells:=True;
-         Range['H'+inttostr(i+1),'J'+inttostr(i+1)].Select;
-         Selection.MergeCells:=True;
-             end;
-         Range['A'+inttostr(i+1),'J'+inttostr(i+1)].Select;
-         Selection.Borders.LineStyle:=1;
-            end
-            else
-             begin
-          Range['A'+inttostr(i+1),'I'+inttostr(i+1)].Insert(xlDown, xlFormatFromLeftOrAbove);
-         Range['A'+inttostr(i+1),'I'+inttostr(i+1)].Select;
-
-         Selection.Borders.LineStyle:=1;
-             end;
-
+          Items[I, 6] := inttostr(FReport.Students[num].balls);
         end;
-     end;
-     inc(num);
+      end;
 
-   end;
-  //   Cells[
+      if I <> first_str + FReport.Students.Count - 1 then
+      begin
+        if (FReport.Is_brs) or (FReport.ik_vid_zan = GOS_EXAM) then
+        begin
+          Range['A' + inttostr(I + 1), 'J' + inttostr(I + 1)
+            ].Insert(xlDown, xlFormatFromLeftOrAbove);
+          if (FReport.ik_vid_zan = GOS_EXAM) then
+          begin
+            Range['B' + inttostr(I + 1), 'D' + inttostr(I + 1)].Select;
+            Selection.MergeCells := True;
+            Range['E' + inttostr(I + 1), 'G' + inttostr(I + 1)].Select;
+            Selection.MergeCells := True;
+            Range['H' + inttostr(I + 1), 'J' + inttostr(I + 1)].Select;
+            Selection.MergeCells := True;
+          end;
+          Range['A' + inttostr(I + 1), 'J' + inttostr(I + 1)].Select;
+          Selection.Borders.LineStyle := 1;
+        end
+        else
+        begin
+          Range['A' + inttostr(I + 1), 'I' + inttostr(I + 1)
+            ].Insert(xlDown, xlFormatFromLeftOrAbove);
+          Range['A' + inttostr(I + 1), 'I' + inttostr(I + 1)].Select;
 
+          Selection.Borders.LineStyle := 1;
+        end;
 
- // NextStep(1, 'Выгружаем отчёт');
+      end;
+    end;
+    inc(num);
+
+  end;
 end;
 
 function TBRS2014VedomostReport.GetTotalSteps: Integer;
 begin
-  Result:=1;
+  Result := 1;
 end;
+
 end.
