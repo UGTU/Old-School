31cae6b5d05b7e180ad76178fb9dfef5c7e34f23
kimspark <sir.k.spark@gmail.com>
2015-02-09 10:42:06 +0300

    commit 523

--- UGTU/Domain/Classes/SpravkaHistory2014.pas
+++ UGTU/Domain/Classes/SpravkaHistory2014.pas
@@ -1,39 +1,45 @@
 unit SpravkaHistory2014;
 
 interface
+
 uses
   Classes, dialogs, SysUtils, ExcelXP, Barcode, Contnrs,
   XIntf, CommonIntf, ReportsBase, db, adodb, ExtCtrls,
   udmUspevaemost, ComCtrls, uAverageBalls,
   System.Generics.Collections;
+
 type
   TSpravkaHistory = class
-    private
+  private
     FNamePric: string;
-    FNumPric:string;
-    Fday :string;
-    Fmonth:string;
-    Fyear :string;
-    protected
+    FNumPric: string;
+    Fday: string;
+    Fmonth: string;
+    Fyear: string;
+  protected
 
-    public
-    property  NamePric : string read  FNamePric    ;
-    property  NumPric : string read  FNumPric    ;
-    property  day : string read  Fday   ;
-    property  month : string read  Fmonth   ;
-    property  year : string read  Fyear   ;
-    public
-    constructor Create (_NumPric,_NamePric,_day,_month,_year:string);
+  public
+    property NamePric: string read FNamePric;
+    property NumPric: string read FNumPric;
+    property day: string read Fday;
+    property month: string read Fmonth;
+    property year: string read Fyear;
+  public
+    constructor Create(_NumPric, _NamePric, _day, _month, _year: string);
 
-    end;
-    implementation
-constructor TSpravkaHistory.Create(_NumPric,_NamePric,_day,_month,_year:string);
+  end;
+
+implementation
+
+constructor TSpravkaHistory.Create(_NumPric, _NamePric, _day, _month,
+  _year: string);
 begin
-  //inherited Create(AOwner);
-        FNumPric:=_NumPric;
-        FNamePric:=_NamePric;
-        Fday:=_day;
-        Fmonth := _month;
-        Fyear:= _year;
+  // inherited Create(AOwner);
+  FNumPric := _NumPric;
+  FNamePric := _NamePric;
+  Fday := _day;
+  Fmonth := _month;
+  Fyear := _year;
 end;
+
 end.
--- UGTU/Presentation/Dialogs/uUniteGroups.pas
+++ UGTU/Presentation/Dialogs/uUniteGroups.pas
@@ -1,19 +1,20 @@
 unit uUniteGroups;
- {#Author sergdev@ist.ugtu.net}
+
+{ #Author sergdev@ist.ugtu.net }
 interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
   Dialogs, uBaseDialog, ActnList, StdCtrls, Buttons, ExtCtrls, ComCtrls,
-  DBTV, DBTVDekanat, DBDekTreeView_TEST,DBTVSpecObj, uMain, DBTVGroupObj,DBTVStudObj,
-  DBGridEh, Mask, DBCtrlsEh, DBLookupEh,uDm, DBTVObj, uDmPrikaz;
+  DBTV, DBTVDekanat, DBDekTreeView_TEST, DBTVSpecObj, uMain, DBTVGroupObj,
+  DBTVStudObj,
+  DBGridEh, Mask, DBCtrlsEh, DBLookupEh, uDm, DBTVObj, uDmPrikaz, System.Actions;
 
 type
   TMoveStud = class(Tobject)
-  stud:TDbNodeStudObject;
-  newGroup:integer;
+    stud: TDbNodeStudObject;
+    newGroup: integer;
   end;
-
 
 type
   TfrmGroupManager = class(TfrmBaseDialog)
@@ -30,31 +31,31 @@ type
     dbcbeOrder: TDBLookupComboboxEh;
     Label4: TLabel;
     Label5: TLabel;
-    procedure FormShow(Sender: TObject);
-    procedure SpeedButton1Click(Sender: TObject);
-    procedure SpeedButton2Click(Sender: TObject);
-    procedure dbcbeNewGroupChange(Sender: TObject);
-    procedure DBDekTreeView_TEST1DblClick(Sender: TObject);
-    procedure lbStudDblClick(Sender: TObject);
-    procedure DBDekTreeView_TEST1Change(Sender: TObject; Node: TTreeNode);
-    procedure lbStudClick(Sender: TObject);
-    procedure SpeedButton4Click(Sender: TObject);
-    procedure SpeedButton3Click(Sender: TObject);
-    procedure actApplyExecute(Sender: TObject);
-    procedure actOKExecute(Sender: TObject);
-    procedure actApplyUpdate(Sender: TObject);
+    procedure FormShow(Sender: Tobject);
+    procedure SpeedButton1Click(Sender: Tobject);
+    procedure SpeedButton2Click(Sender: Tobject);
+    procedure dbcbeNewGroupChange(Sender: Tobject);
+    procedure DBDekTreeView_TEST1DblClick(Sender: Tobject);
+    procedure lbStudDblClick(Sender: Tobject);
+    procedure DBDekTreeView_TEST1Change(Sender: Tobject; Node: TTreeNode);
+    procedure lbStudClick(Sender: Tobject);
+    procedure SpeedButton4Click(Sender: Tobject);
+    procedure SpeedButton3Click(Sender: Tobject);
+    procedure actApplyExecute(Sender: Tobject);
+    procedure actOKExecute(Sender: Tobject);
+    procedure actApplyUpdate(Sender: Tobject);
 
   private
-  procedure Refreshlist;
-   procedure RemovefromList();
+    procedure Refreshlist;
+    procedure RemovefromList();
     procedure AddCurrentToList;
     procedure AddGroupToList;
     procedure RemoveGroupFromList;
     { Private declarations }
   public
 
-  MovedList:Tlist;
-  spec:TDBNodeSpecObject;
+    MovedList: Tlist;
+    spec: TDBNodeSpecObject;
     { Public declarations }
   end;
 
@@ -67,227 +68,249 @@ uses Math, uDMGroupActions, uDMUgtuStructure;
 
 {$R *.dfm}
 
-
 procedure TfrmGroupManager.Refreshlist;
-var i:integer;
-cur:TMoveStud;
+var
+  i: integer;
+  cur: TMoveStud;
 begin
-lbStud.Items.Clear;
-for i:=0 to (MovedList.Count-1) do
-begin
-  cur:=MovedList.items[i];
-  if cur.newGroup=dbcbeNewGroup.KeyValue then
-  lbStud.AddItem(cur.stud.LastName+' '+cur.stud.Name+' '+cur.stud.LastName,cur);
-end
+  lbStud.Items.Clear;
+  for i := 0 to (MovedList.Count - 1) do
+  begin
+    cur := MovedList.Items[i];
+    if cur.newGroup = dbcbeNewGroup.KeyValue then
+      lbStud.AddItem(cur.stud.LastName + ' ' + cur.stud.Name + ' ' +
+        cur.stud.LastName, cur);
+  end
 end;
-
 
 procedure TfrmGroupManager.AddCurrentToList;
-var newMove:TMoveStud;
-i:integer;
+var
+  newMove: TMoveStud;
+  i: integer;
 begin
-if (dbcbeNewGroup.KeyValue<=0)or(TDBNodeObject(DBDekTreeView_TEST1.Selected.Data) is TDbNodeGroupObject) then exit;
+  if (dbcbeNewGroup.KeyValue <= 0) or
+    (TDBNodeObject(DBDekTreeView_TEST1.Selected.Data) is TDbNodeGroupObject)
+  then
+    exit;
 
-if (DBDekTreeView_TEST1.Selected.ImageIndex=31) then
-begin
-  for i:= 0 to (MovedList.Count-1) do
+  if (DBDekTreeView_TEST1.Selected.ImageIndex = 31) then
   begin
-    newMove:=MovedList[i];
-    if (newMove.stud=DBDekTreeView_TEST1.SelectedObject) then
+    for i := 0 to (MovedList.Count - 1) do
     begin
-      newMove.stud.Node.ImageIndex:=newMove.stud.baseImageIndex;
-      MovedList.Remove(newMove);
-      RefreshList;
-      exit;
+      newMove := MovedList[i];
+      if (newMove.stud = DBDekTreeView_TEST1.SelectedObject) then
+      begin
+        newMove.stud.Node.ImageIndex := newMove.stud.baseImageIndex;
+        MovedList.Remove(newMove);
+        Refreshlist;
+        exit;
+      end;
     end;
+    Refreshlist;
+    exit;
   end;
-RefreshList;
-exit;
-end;
 
-newMove:=TMoveStud.Create;
-newMove.stud:=DBDekTreeView_TEST1.Selected.Data;
-newMove.newGroup:=dbcbeNewGroup.KeyValue;
-MovedList.Add(newMove);
-DBDekTreeView_TEST1.Selected.ImageIndex:=31;
-RefreshList;
+  newMove := TMoveStud.Create;
+  newMove.stud := DBDekTreeView_TEST1.Selected.Data;
+  newMove.newGroup := dbcbeNewGroup.KeyValue;
+  MovedList.Add(newMove);
+  DBDekTreeView_TEST1.Selected.ImageIndex := 31;
+  Refreshlist;
 
 end;
 
 procedure TfrmGroupManager.RemovefromList;
-var cur:TMoveStud;
+var
+  cur: TMoveStud;
 begin
-if lbStud.ItemIndex<0 then exit;
-cur:=TMoveStud(lbStud.items.Objects[lbStud.ItemIndex]);
-cur.stud.Node.ImageIndex:=cur.stud.baseImageIndex;
-MovedList.Remove(cur);
-cur.Free;
+  if lbStud.ItemIndex < 0 then
+    exit;
+  cur := TMoveStud(lbStud.Items.Objects[lbStud.ItemIndex]);
+  cur.stud.Node.ImageIndex := cur.stud.baseImageIndex;
+  MovedList.Remove(cur);
+  cur.Free;
 
-RefreshList;
+  Refreshlist;
 end;
 
-
-procedure TfrmGroupManager.FormShow(Sender: TObject);
-var Node:TTreeNode;
-DBNode:TDBNodeSpecObject;
+procedure TfrmGroupManager.FormShow(Sender: Tobject);
+var
+  Node: TTreeNode;
+  DBNode: TDBNodeSpecObject;
 begin
-MovedList:=TList.create;
-dmUgtuStructure.adodsGroupsofspec.Active:=false;
-dmUgtuStructure.adodsGroupsofspec.CommandText:='select * from Tree_groups where ik_spec_fac='''+inttostr(spec.ik)+'''';
-dmUgtuStructure.adodsGroupsofspec.Active:=true;
-dmPrikaz.adodsPrikaz.active:=true;
+  MovedList := Tlist.Create;
+  dmUgtuStructure.adodsGroupsofspec.Active := false;
+  dmUgtuStructure.adodsGroupsofspec.CommandText :=
+    'select * from Tree_groups where ik_spec_fac=''' + inttostr(spec.ik) + '''';
+  dmUgtuStructure.adodsGroupsofspec.Active := true;
+  dmPrikaz.adodsPrikaz.Active := true;
 
-Node:=DBDekTreeView_TEST1.CreateNewNode(nil, spec.Name, TDBNodeSpecObject);
+  Node := DBDekTreeView_TEST1.CreateNewNode(nil, spec.Name, TDBNodeSpecObject);
 
-DBnode:=TDBNodeSpecObject.create(Node,DBDekTreeView_TEST1);
-DbNode.StudyYears:=spec.StudyYears;
-DBNode.ik:=spec.ik;
-DBNode.ShortName:=spec.ShortName;
-DBNode.Name:=spec.Name;
-DBnode.ShowActual:=spec.showActual;
-DBnode.SetAdoDataset(spec.AdoDataset.Connection);
-DBnode.BaseImageIndex:=5;
-DBNode.ShowStudents:=ssShowActualOnly;
-Node.Data:=DBnode;
-Node.ImageIndex:=DBNode.BaseImageIndex;
-DBNode.AddChildNodes;
-
-end;
-
-
-procedure TfrmGroupManager.SpeedButton1Click(Sender: TObject);
-begin
-AddCurrenttolist;
-end;
-
-procedure TfrmGroupManager.SpeedButton2Click(Sender: TObject);
-begin
-RemovefromList;
+  DBNode := TDBNodeSpecObject.Create(Node, DBDekTreeView_TEST1);
+  DBNode.StudyYears := spec.StudyYears;
+  DBNode.ik := spec.ik;
+  DBNode.ShortName := spec.ShortName;
+  DBNode.Name := spec.Name;
+  DBNode.ShowActual := spec.ShowActual;
+  DBNode.SetAdoDataset(spec.AdoDataset.Connection);
+  DBNode.baseImageIndex := 5;
+  DBNode.ShowStudents := ssShowActualOnly;
+  Node.Data := DBNode;
+  Node.ImageIndex := DBNode.baseImageIndex;
+  DBNode.AddChildNodes;
 
 end;
 
-procedure TfrmGroupManager.dbcbeNewGroupChange(Sender: TObject);
+procedure TfrmGroupManager.SpeedButton1Click(Sender: Tobject);
 begin
-Refreshlist;
-if (dbcbeNewGroup.KeyValue<>Null)and(frmMain.DBDekTreeView_TEST1.Selectedobject is TDBNodeGroupObject) then
-SpeedButton4.Enabled:=true;
-
-if (dbcbeNewGroup.KeyValue<>Null)and(lbStud.Items.Count>0) then
-SpeedButton3.Enabled:=true else
-SpeedButton3.Enabled:=false;
+  AddCurrentToList;
 end;
 
-procedure TfrmGroupManager.DBDekTreeView_TEST1DblClick(Sender: TObject);
+procedure TfrmGroupManager.SpeedButton2Click(Sender: Tobject);
 begin
-AddCurrenttolist;
-end;
-
-procedure TfrmGroupManager.lbStudDblClick(Sender: TObject);
-begin
-RemovefromList
+  RemovefromList;
 
 end;
 
-procedure TfrmGroupManager.DBDekTreeView_TEST1Change(Sender: TObject;
+procedure TfrmGroupManager.dbcbeNewGroupChange(Sender: Tobject);
+begin
+  Refreshlist;
+  if (dbcbeNewGroup.KeyValue <> Null) and
+    (frmMain.DBDekTreeView_TEST1.SelectedObject is TDbNodeGroupObject) then
+    SpeedButton4.Enabled := true;
+
+  if (dbcbeNewGroup.KeyValue <> Null) and (lbStud.Items.Count > 0) then
+    SpeedButton3.Enabled := true
+  else
+    SpeedButton3.Enabled := false;
+end;
+
+procedure TfrmGroupManager.DBDekTreeView_TEST1DblClick(Sender: Tobject);
+begin
+  AddCurrentToList;
+end;
+
+procedure TfrmGroupManager.lbStudDblClick(Sender: Tobject);
+begin
+  RemovefromList;
+end;
+
+procedure TfrmGroupManager.DBDekTreeView_TEST1Change(Sender: Tobject;
   Node: TTreeNode);
 begin
-if (DBDekTreeView_TEST1.SelectedObject is TDBNodeStudObject) then
-begin
-SpeedButton4.Enabled:=false;
-if dbcbeNewGroup.KeyValue<>Null then SpeedButton1.Enabled:=true;
-end else
-begin
-SpeedButton1.Enabled:=false;
-if dbcbeNewGroup.KeyValue<>Null then SpeedButton4.Enabled:=true;
-end;
+  if (DBDekTreeView_TEST1.SelectedObject is TDbNodeStudObject) then
+  begin
+    SpeedButton4.Enabled := false;
+    if dbcbeNewGroup.KeyValue <> Null then
+      SpeedButton1.Enabled := true;
+  end
+  else
+  begin
+    SpeedButton1.Enabled := false;
+    if dbcbeNewGroup.KeyValue <> Null then
+      SpeedButton4.Enabled := true;
+  end;
 
 end;
 
-procedure TfrmGroupManager.lbStudClick(Sender: TObject);
+procedure TfrmGroupManager.lbStudClick(Sender: Tobject);
 begin
- if lbStud.ItemIndex<0 then
- SpeedButton2.Enabled:=false else
- SpeedButton2.Enabled:=true;
- if lbStud.Items.count>0 then
- SpeedButton3.Enabled:=true else
- SpeedButton3.Enabled:=false;
+  if lbStud.ItemIndex < 0 then
+    SpeedButton2.Enabled := false
+  else
+    SpeedButton2.Enabled := true;
+  if lbStud.Items.Count > 0 then
+    SpeedButton3.Enabled := true
+  else
+    SpeedButton3.Enabled := false;
 end;
 
 procedure TfrmGroupManager.AddGroupToList;
-var i:integer;
-newMove:TMoveStud;
+var
+  i: integer;
+  newMove: TMoveStud;
 begin
-//if (dbcbeNewGroup.KeyValue<=0)or(DBDekTreeView_TEST1.Selected.ImageIndex=31)or(TDBNodeObject(DBDekTreeView_TEST1.Selected.Data) is TDbNodeGroupObject) then exit;
+  // if (dbcbeNewGroup.KeyValue<=0)or(DBDekTreeView_TEST1.Selected.ImageIndex=31)or(TDBNodeObject(DBDekTreeView_TEST1.Selected.Data) is TDbNodeGroupObject) then exit;
 
-For i:=0 to (DBDekTreeView_TEST1.Selected.Count-1) do begin
-newMove:=TMoveStud.Create;
-newMove.stud:=DBDekTreeView_TEST1.Selected.Item[i].data;
-newMove.newGroup:=dbcbeNewGroup.KeyValue;
-if not (newMove.stud.Node.ImageIndex=31) then
-MovedList.Add(newMove);
-newMove.stud.Node.ImageIndex:=31;
-end;
+  For i := 0 to (DBDekTreeView_TEST1.Selected.Count - 1) do
+  begin
+    newMove := TMoveStud.Create;
+    newMove.stud := DBDekTreeView_TEST1.Selected.Item[i].Data;
+    newMove.newGroup := dbcbeNewGroup.KeyValue;
+    if not(newMove.stud.Node.ImageIndex = 31) then
+      MovedList.Add(newMove);
+    newMove.stud.Node.ImageIndex := 31;
+  end;
 
-RefreshList;
+  Refreshlist;
 end;
 
 procedure TfrmGroupManager.RemoveGroupFromList;
-var i:integer;
-cur:TMoveStud;
+var
+  i: integer;
+  cur: TMoveStud;
 begin
-if lbStud.items.count=0 then exit;
+  if lbStud.Items.Count = 0 then
+    exit;
 
-for i:= 0 to (lbStud.items.count-1) do
-begin
-cur:=TMoveStud(lbStud.items.Objects[i]);
-cur.stud.Node.ImageIndex:=cur.stud.baseImageIndex;
-MovedList.Remove(cur);
-cur.Free;
-end;
+  for i := 0 to (lbStud.Items.Count - 1) do
+  begin
+    cur := TMoveStud(lbStud.Items.Objects[i]);
+    cur.stud.Node.ImageIndex := cur.stud.baseImageIndex;
+    MovedList.Remove(cur);
+    cur.Free;
+  end;
 
-RefreshList;
-
-end;
-
-procedure TfrmGroupManager.SpeedButton4Click(Sender: TObject);
-begin
-AddGroupToList;
+  Refreshlist;
 
 end;
 
-procedure TfrmGroupManager.SpeedButton3Click(Sender: TObject);
+procedure TfrmGroupManager.SpeedButton4Click(Sender: Tobject);
 begin
-RemoveGroupFromList;
+  AddGroupToList;
 
 end;
 
-procedure TfrmGroupManager.actApplyExecute(Sender: TObject);
-var i:integer;
-cur:TMoveStud;
+procedure TfrmGroupManager.SpeedButton3Click(Sender: Tobject);
 begin
-for i:=0 to (MovedList.Count-1) do begin
-   cur:=MovedList[i];
-   dmGroupActions.aspGroupChange_withupdate.Active:=false;
-   dmGroupActions.aspGroupChange_withupdate.Parameters[1].Value:=cur.newGroup;
-   dmGroupActions.aspGroupChange_withupdate.Parameters[2].Value:=cur.stud.RecordbookKey;
-   dmGroupActions.aspGroupChange_withupdate.Parameters[3].Value:=dbcbeOrder.KeyValue;
-   dmGroupActions.aspGroupChange_withupdate.ExecProc;
-   Refreshlist;
-end;
-MovedList.Clear;
-Refreshlist;
+  RemoveGroupFromList;
+
 end;
 
-procedure TfrmGroupManager.actApplyUpdate(Sender: TObject);
+procedure TfrmGroupManager.actApplyExecute(Sender: Tobject);
+var
+  i: integer;
+  cur: TMoveStud;
 begin
-actApply.Enabled:=not ((dbcbeNewGroup.Text=''));//or(dbcbeOrder.Text=''));
-actOk.Enabled:=not ((dbcbeNewGroup.Text=''));//or(dbcbeOrder.Text=''));
+  for i := 0 to (MovedList.Count - 1) do
+  begin
+    cur := MovedList[i];
+    dmGroupActions.aspGroupChange_withupdate.Active := false;
+    dmGroupActions.aspGroupChange_withupdate.Parameters[1].Value :=
+      cur.newGroup;
+    dmGroupActions.aspGroupChange_withupdate.Parameters[2].Value :=
+      cur.stud.RecordbookKey;
+    dmGroupActions.aspGroupChange_withupdate.Parameters[3].Value :=
+      dbcbeOrder.KeyValue;
+    dmGroupActions.aspGroupChange_withupdate.ExecProc;
+    Refreshlist;
+  end;
+  MovedList.Clear;
+  Refreshlist;
 end;
 
-procedure TfrmGroupManager.actOKExecute(Sender: TObject);
+procedure TfrmGroupManager.actApplyUpdate(Sender: Tobject);
 begin
-actApplyExecute(Sender);
-Close;
+  actApply.Enabled := not((dbcbeNewGroup.Text = ''));
+  // or(dbcbeOrder.Text=''));
+  actOk.Enabled := not((dbcbeNewGroup.Text = '')); // or(dbcbeOrder.Text=''));
+end;
+
+procedure TfrmGroupManager.actOKExecute(Sender: Tobject);
+begin
+  actApplyExecute(Sender);
+  Close;
 end;
 
 end.
