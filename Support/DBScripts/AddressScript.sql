6a66b84dc43af536613ed68ee8dfeae4f2036abf
kimspark <sir.k.spark@gmail.com>
2015-04-09 13:24:19 +0300

    commit

--- Support/DBScripts/AddressScript.sql
+++ Support/DBScripts/AddressScript.sql
@@ -36,15 +36,16 @@ order by alum_count
 alter VIEW [dbo].[CitySchoolAlumni]
 AS
 select Gorod.Cgorod, count(distinct Person.nCode) alum_count, Gorod.Latitude, Gorod.Longitude,
-  count(Zach.Ik_zach) zach_count, NNyear
+  count(Zach.Ik_zach) zach_count, NNyear, ik_spec
 from ABIT_postup inner join Person on ABIT_postup.nCode = Person.nCode
 inner join Student on Student.nCode = Person.nCode
 inner join ABIT_Diapazon_spec_fac on ABIT_Diapazon_spec_fac.NNrecord = ABIT_postup.NNrecord
+inner join Relation_spec_fac on Relation_spec_fac.ik_spec_fac = ABIT_Diapazon_spec_fac.ik_spec_fac
 inner join Zaved_stud on Student.Ik_zaved = Zaved_stud.ik_zaved
 inner join Gorod on Gorod.Ik_gorod = Zaved_stud.ik_gorod
 left join Zach on Zach.nCode = Person.nCode
 where (Gorod.Longitude is not null) and (Gorod.Latitude is not null) and NNyear<2015
-group by Gorod.Cgorod, Gorod.Latitude, Gorod.Longitude, NNyear 
+group by Gorod.Cgorod, Gorod.Latitude, Gorod.Longitude, NNyear, ik_spec 
 GO
 
 select * from CitySchoolAlumni order by NNyear, alum_count
