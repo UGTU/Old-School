e703ad35d83ec5d2bdd2e3a967141583ea0b4e0a
kimspark <sir.k.spark@gmail.com>
2015-07-30 09:10:22 +0300

    scripts

--- Support/DBScripts/Для банков/Зачисленные Высшее (Газпромбанк).sql
+++ Support/DBScripts/Для банков/Зачисленные Высшее (Газпромбанк).sql
@@ -2,23 +2,24 @@
 
 select distinct 
 	   Cshort_name_fac,
-	   Cname_spec
+	   Cname_spec,
+	   cName_direction,
        '',
 	   43,
 	   Clastname,
 	   Cfirstname,
 	   Cotch,
 	   iif(lSex = 0,'Ж','М') lSex,
-	   pr.Cstrana,
+	   isnull(pr.Cstrana,vrem.Cstrana) strana,
 	   '' Pr_index,
-	   pr.Cregion,
-	   pr.Craion,
-	   '' CGorod,
-	   pr.Cgorod NPunkt,
-	   pr.CStreet,
-	   iif(pr.b_i>0,substring(pr.BuildingNumber,1,pr.b_i - 1),pr.BuildingNumber) dom,
-	   iif(pr.b_i>0,substring(pr.BuildingNumber,pr.b_i,1),pr.StructNumber) korpus,
-	   pr.FlatNumber,
+	   isnull(pr.Cregion,vrem.Cregion) region,
+	   isnull(pr.Craion,vrem.Craion) raion,
+	   '' Gorod,
+	    isnull(pr.Cgorod,vrem.Cgorod) NPunkt,
+	   isnull(pr.CStreet,vrem.CStreet) Street,
+	   isnull(iif(pr.b_i>0,substring(pr.BuildingNumber,1,pr.b_i - 1),pr.BuildingNumber),iif(vrem.b_i>0,substring(vrem.BuildingNumber,1,pr.b_i - 1),vrem.BuildingNumber)) dom,
+	   isnull(iif(pr.b_i>0,substring(pr.BuildingNumber,pr.b_i,1),pr.StructNumber),iif(vrem.b_i>0,substring(vrem.BuildingNumber,vrem.b_i,1),vrem.StructNumber)) korpus,
+	   isnull(pr.FlatNumber,vrem.FlatNumber) FlatNumber,
 	   docs.cvid_doc,
 	   cast(Cd_seria as varchar(10)) Cd_seria,
 	   cast(Np_number as varchar(20)) Np_number,
@@ -56,6 +57,7 @@ from
 		   cEmail,
 		   Fac.Cshort_name_fac,
 		   Spec_stud.Cname_spec,
+		   Direction.cName_direction,
 		   Person.nCode,
 		   Person.Ik_grazd,
 		   Strana.Cstrana c_grazd,
@@ -64,7 +66,7 @@ from
 		   Cname_form_ed,
 		   CType_kat
 	from dbo.Person,ABIT_postup,ABIT_Diapazon_spec_fac,dbo.Student,Relation_spec_fac,Spec_stud,Fac,
-		 Strana,Form_ed,Kat_zach,TypeKatZach
+		 Strana,Form_ed,Kat_zach,TypeKatZach,Direction
 	where ABIT_postup.nCode = dbo.Person.nCode and ABIT_Diapazon_spec_fac.NNrecord = ABIT_postup.NNrecord
 	and ABIT_Diapazon_spec_fac.ik_spec_fac = Relation_spec_fac.ik_spec_fac
 	and ABIT_postup.ik_kat_zach = Kat_zach.Ik_kat_zach
@@ -72,9 +74,11 @@ from
 	and Person.Ik_grazd = Strana.Ik_strana
 	and Form_ed.Ik_form_ed=Relation_spec_fac.Ik_form_ed
 	and Relation_spec_fac.ik_fac = Fac.Ik_fac and Relation_spec_fac.ik_spec = Spec_stud.ik_spec
+	and Spec_stud.ik_direction = Direction.ik_direction
 	--and ik_zach in (select ik_zach from ABIT_sost_zach where ik_type_zach = 2) --зачисленные
-	--and Relation_spec_fac.Ik_form_ed = 1									   --очники
-	and Relation_spec_fac.ik_fac <> 31										  --колледжи
+	and Relation_spec_fac.Ik_form_ed = 1									--очники
+	and TypeKatZach.ik_type_kat in (1,2,12)									--не контракт
+	and Relation_spec_fac.ik_fac <> 31										--не колледжи
 	and dbo.Student.nCode = ABIT_postup.nCode
 	and NNyear=year(GETDATE())
 ) stud
@@ -102,7 +106,8 @@ left join (select nCode,FlatNumber,StructNumber,BuildingNumber,CStreet,Cgorod,Cr
 		   and dbo.Raion.Ik_region = dbo.Region.Ik_region
 		   and dbo.Region.Ik_strana = dbo.Strana.Ik_strana) fac
 on stud.nCode = fac.nCode
-left join (select nCode,FlatNumber,StructNumber,BuildingNumber,CStreet,Cgorod,Cregion,Cstrana,Strana.Ik_strana
+left join (select nCode,FlatNumber,StructNumber,BuildingNumber,CStreet,Cgorod,Cregion,Craion,Cstrana,Strana.Ik_strana,
+		   PATINDEX('%[А-я]%',BuildingNumber) b_i
 		   from dbo.PersonAddress,dbo.Address,dbo.Street,dbo.Gorod,dbo.Raion,dbo.Region,dbo.Strana
 		   where ik_AddressType=3
 		   and dbo.Address.ik_address = dbo.PersonAddress.ik_address
@@ -112,5 +117,6 @@ left join (select nCode,FlatNumber,StructNumber,BuildingNumber,CStreet,Cgorod,Cr
 		   and dbo.Raion.Ik_region = dbo.Region.Ik_region
 		   and dbo.Region.Ik_strana = dbo.Strana.Ik_strana) vrem
 on stud.nCode = vrem.nCode
---where ik_grazd <> 2
+--where (pr.Cstrana is null)and(vrem.Cstrana is null)
+--where Dd_vidan is null
 order by  Clastname,Cfirstname,Cotch
--- Support/DBScripts/Для банков/Зачисленные СПО (Сбербанк).sql
+++ Support/DBScripts/Для банков/Зачисленные СПО (Сбербанк).sql
@@ -15,21 +15,21 @@ select distinct
 	   Cplacebirth,
 	   iif(lSex = 0,'Ж','М') lSex,
 	   '' Pr_index,
-	   pr.Cstrana,
+	   isnull(pr.Cstrana,vrem.Cstrana) Cstrana,
 	   '' code_strana,
-	   pr.Cregion,
+	   isnull(pr.Cregion,vrem.Cregion) Cregion,
 	   '' type_region,
-	   pr.Craion,
+	   isnull(pr.Craion,vrem.Craion) Craion,
 	   '' type_raion,
-	   pr.Cgorod,
+	   isnull(pr.Cgorod,vrem.Cgorod) Cgorod,
 	   '' type_gorod,
 	   '' CPunkt,
 	   '' type_punkt,
-	   pr.CStreet,
+	   isnull(pr.CStreet,vrem.CStreet) CStreet,
 	   '' type_street,
-	   iif(pr.b_i>0,substring(pr.BuildingNumber,1,pr.b_i - 1),pr.BuildingNumber) dom,
-	   pr.FlatNumber,
-	   iif(pr.b_i>0,substring(pr.BuildingNumber,pr.b_i,1),pr.StructNumber) korpus
+	   isnull(iif(pr.b_i>0,substring(pr.BuildingNumber,1,pr.b_i - 1),pr.BuildingNumber),iif(vrem.b_i>0,substring(vrem.BuildingNumber,1,vrem.b_i - 1),vrem.BuildingNumber)) dom,
+	   isnull(pr.FlatNumber,vrem.FlatNumber) FlatNumber,
+	   isnull(iif(pr.b_i>0,substring(pr.BuildingNumber,pr.b_i,1),pr.StructNumber),iif(vrem.b_i>0,substring(vrem.BuildingNumber,vrem.b_i,1),vrem.StructNumber)) korpus
 from
 (
 	select distinct cast(dd_pod_zayav as Date) dd_pod_zayav,
@@ -44,6 +44,7 @@ from
 		   cSotTel SotTel,
 		   Fac.Cshort_name_fac,
 		   Spec_stud.Cname_spec,
+		   Direction.cName_direction,
 		   Person.nCode,
 		   Person.Ik_grazd,
 		   Strana.Cstrana c_grazd,
@@ -52,7 +53,7 @@ from
 		   Cname_form_ed,
 		   CType_kat
 	from dbo.Person,ABIT_postup,ABIT_Diapazon_spec_fac,dbo.Student,Relation_spec_fac,Spec_stud,Fac,
-		 Strana,Form_ed,Kat_zach,TypeKatZach
+		 Strana,Form_ed,Kat_zach,TypeKatZach,Direction
 	where ABIT_postup.nCode = dbo.Person.nCode and ABIT_Diapazon_spec_fac.NNrecord = ABIT_postup.NNrecord
 	and ABIT_Diapazon_spec_fac.ik_spec_fac = Relation_spec_fac.ik_spec_fac
 	and ABIT_postup.ik_kat_zach = Kat_zach.Ik_kat_zach
@@ -60,9 +61,11 @@ from
 	and Person.Ik_grazd = Strana.Ik_strana
 	and Form_ed.Ik_form_ed=Relation_spec_fac.Ik_form_ed
 	and Relation_spec_fac.ik_fac = Fac.Ik_fac and Relation_spec_fac.ik_spec = Spec_stud.ik_spec
+	and Spec_stud.ik_direction = Direction.ik_direction
 	--and ik_zach in (select ik_zach from ABIT_sost_zach where ik_type_zach = 2) --зачисленные
-	--and Relation_spec_fac.Ik_form_ed = 1									   --очники
-	and Relation_spec_fac.ik_fac = 31										  --колледжи
+	and Relation_spec_fac.Ik_form_ed = 1									--очники
+	and TypeKatZach.ik_type_kat in (1,2,12)									--не контракт
+	and Relation_spec_fac.ik_fac = 31										--колледжи
 	and dbo.Student.nCode = ABIT_postup.nCode
 	and NNyear=year(GETDATE())
 ) stud
@@ -90,7 +93,8 @@ left join (select nCode,FlatNumber,StructNumber,BuildingNumber,CStreet,Cgorod,Cr
 		   and dbo.Raion.Ik_region = dbo.Region.Ik_region
 		   and dbo.Region.Ik_strana = dbo.Strana.Ik_strana) fac
 on stud.nCode = fac.nCode
-left join (select nCode,FlatNumber,StructNumber,BuildingNumber,CStreet,Cgorod,Cregion,Cstrana,Strana.Ik_strana
+left join (select nCode,FlatNumber,StructNumber,BuildingNumber,CStreet,Cgorod,Cregion,Craion,Cstrana,Strana.Ik_strana,
+		   PATINDEX('%[А-я]%',BuildingNumber) b_i
 		   from dbo.PersonAddress,dbo.Address,dbo.Street,dbo.Gorod,dbo.Raion,dbo.Region,dbo.Strana
 		   where ik_AddressType=3
 		   and dbo.Address.ik_address = dbo.PersonAddress.ik_address
@@ -100,5 +104,6 @@ left join (select nCode,FlatNumber,StructNumber,BuildingNumber,CStreet,Cgorod,Cr
 		   and dbo.Raion.Ik_region = dbo.Region.Ik_region
 		   and dbo.Region.Ik_strana = dbo.Strana.Ik_strana) vrem
 on stud.nCode = vrem.nCode
---where ik_grazd <> 2
+--where (pr.Cstrana is null)and(vrem.Cstrana is null)
+--where Dd_vidan is null
 order by  Clastname,Cfirstname,Cotch
